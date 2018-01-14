0f44c2fe4dc786fb04402835d7375513c805d215
Patryk Omiotek <patryk.omiotek@gmail.com>
2018-01-14 10:14:22 +0100

    refactor

--- src/Counter/logic.js
+++ src/Counter/logic.js
@@ -43,15 +43,13 @@ const INITIAL_STATE = {
 export default function reducer(state = INITIAL_STATE, action) {
   switch (action.type) {
     case ADD:
-      return Object.assign({}, state, { counterValue: state.counterValue + 1 });
+      return { counterValue: state.counterValue + 1 };
     case SUB:
-      return Object.assign({}, state, { counterValue: state.counterValue - 1 });
+      return { counterValue: state.counterValue - 1 };
     case RESET:
-      return Object.assign({}, state, { counterValue: 0 });
+      return { counterValue: 0 };
     case ADD_VALUE:
-      return Object.assign({}, state, {
-        counterValue: state.counterValue + action.value
-      });
+      return { counterValue: state.counterValue + action.value };
     default:
       return state;
   }
